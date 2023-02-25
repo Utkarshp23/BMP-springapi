475f91f87dc1f83245aa16f498847ef7cde86810
Mhaske1200 <mhaskeshubham1200@gmail.com>
2023-02-25 13:00:09 +0530

    Initial Project Setup

---
+++ src/main/java/com/bmp/backend/BmpRestSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.bmp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmpRestSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmpRestSpringBootApplication.class, args);
+	}
+
+}
