f6006067aa7383c20b7291e44bd63bdc12c32e39
Mhaske Shubham <mhaskeshubham1200@gmail.com>
2023-03-03 18:09:51 +0530

    Property Add Remove Show

---
+++ src/main/java/com/bmp/backend/controllers/PropertyController.java
@@ -0,0 +1,44 @@
+package com.bmp.backend.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bmp.backend.entities.Property;
+import com.bmp.backend.entities.User;
+import com.bmp.backend.entities.UserCategory;
+import com.bmp.backend.services.PropertyService;
+import com.bmp.backend.services.UserService;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+public class PropertyController 
+{
+	
+	@Autowired
+    PropertyService propService;
+	
+	@GetMapping("/getproperty")
+    public List<Property> getProperty()
+    {
+        return propService.getProperty();
+    }
+	
+	@PostMapping("/addproperty")
+    public Property addProperty(@RequestBody Property p)
+    {
+        return propService.addProperty(p);
+    }
+	
+	@PostMapping("/deleteproperty")
+    public int delProperty(@RequestBody Property p)
+    {
+        return propService.delProperty(p.getPid());
+    }
+
+}
